05eca41a714f64d04f5ff1a9d71df7dc7aefee4e
ankit <>
2020-07-06 10:32:14 -0600

    Spring Security using JDBC Authentication and H2 DB

--- SpringSecurityDemo/src/main/java/com/example/demo/SecurityConfiguration.java
+++ SpringSecurityDemo/src/main/java/com/example/demo/SecurityConfiguration.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
@@ -7,18 +10,19 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	DataSource dataSource; 
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-		.withUser("admin").password("admin").roles("admin")
-		.and()
-		.withUser("user").password("user").roles("user");
+		auth.jdbcAuthentication().dataSource(dataSource);
 	}
 
 	@Bean
@@ -30,7 +34,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 		.antMatchers("/admin").hasRole("admin")
-		.antMatchers("/user").hasRole("user")
+		.antMatchers("/user").hasAnyRole("user","admin")
 		.antMatchers("/").permitAll()
 		.and().formLogin();
 	}
---
+++ SpringSecurityDemo/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into users values ('user','user',true);
+insert into authorities values('user','ROLE_USER');
+
+insert into users values ('admin','admin',true);
+insert into authorities values('admin','ROLE_ADMIN');
+
+insert into users values ('ankit','ankit',true);
+insert into authorities values('user','ROLE_USER');
